7201c4c82f457ea360a61cfd99b0888670536001
PetERPAN17 <peterpan17.dev@gmail.com>
2020-10-04 17:21:18 +0900

    Change

--- scraping.py
+++ scraping.py
@@ -1,39 +1,53 @@
 import requests # pip install requests
 import bs4 # pip install beautifulsoup4
 
-# request WEB page
-response = requests.get('https://7gogo.jp/tanaka-miku/32070')
+nanagogoUrl = 'https://7gogo.jp/'
 
-# Confirmation status
-print(response.status_code)
+# Get Member List
+members = ['tanaka-miku']
 
-# Stop except status 200
-response.raise_for_status()
+# Loop Members
+for member in  members:
 
-# Analisys HTML
-soup = bs4.BeautifulSoup(response.text , "html.parser" )
+    # Get last number processed
+    lastNumber = 32069
 
-# print(soup.main)
+    # Set now number
+    nowNumber = lastNumber + 1
 
-# Get certain class elements in analisys HTML
-# elem = soup.select('._3DSDHo6-._2icsf9K-')
+    # request WEB page
+    response = requests.get(nanagogoUrl + member + '/' + repr(nowNumber))
 
-# Get video Tag element in certain class elements in analisys HTML
-videos = soup.select('._3DSDHo6-._2icsf9K- video')
-#for video in videos:
-    #print(video.get('src'))
+    # Confirmation status
+    print(response.status_code)
 
-# Get img Tag element in certain class elements in analisys HTML
-imgs = soup.select('._3DSDHo6-._2icsf9K- img')
-for img in imgs:
-    #print(img)git
-    print(img.get('data-src'))
+    # Stop except status 200
+    response.raise_for_status()
 
-# Create file
-#with open('./miku.html', 'w') as file:
-    #for elem in elems:
-        #print(str(elem))
-        # Write text into file
-        #file.write(str(elem))
+    # Analisys HTML
+    soup = bs4.BeautifulSoup(response.text , "html.parser" )
 
-    #file.write(str(elem)) # Write text into file
+    # print(soup.main)
+
+    # Get certain class elements in analisys HTML
+    # elem = soup.select('._3DSDHo6-._2icsf9K-')
+
+    # Get video Tag element in certain class elements in analisys HTML
+    videos = soup.select('._3DSDHo6-._2icsf9K- video')
+    #for video in videos:
+        #print(video.get('src'))
+
+    # Get img Tag element in certain class elements in analisys HTML
+    imgs = soup.select('._3DSDHo6-._2icsf9K- img')
+    for img in imgs:
+        #print(img)git
+        print(img.get('data-src'))
+
+    # Create file
+    #with open('./miku.html', 'w') as file:
+        #for elem in elems:
+            #print(str(elem))
+            # Write text into file
+            #file.write(str(elem))
+
+        #file.write(str(elem)) # Write text into file
